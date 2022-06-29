5eea5b7228907b94afc59d7999a91b9679b6fc5a
dead-playmod <playmod.pl@gmail.com>
2022-06-29 19:30:02 +0200

    webpack + addToCartExtension

---
+++ assets/shop/addToCartExtension.js
@@ -0,0 +1,36 @@
+import $ from 'jquery';
+
+$.fn.extend({
+  addToCart() {
+    const element = this;
+    const url = $(element).attr('action');
+    const validationElement = $('#sylius-cart-validation-error');
+
+    element.api({
+      method: 'POST',
+      on: 'submit',
+      cache: false,
+      url,
+      beforeSend(settings) {
+        /* eslint-disable-next-line no-param-reassign */
+        settings.data = element.serialize();
+
+        return settings;
+      },
+      onSuccess() {
+        validationElement.addClass('hidden');
+        window.location.reload();
+      },
+      onFailure(response) {
+        validationElement.removeClass('hidden');
+        let validationMessage = '';
+
+        Object.entries(response.errors.errors).forEach(([, message]) => {
+          validationMessage += message;
+        });
+        validationElement.html(validationMessage);
+        $(element).removeClass('loading');
+      },
+    });
+  },
+});
